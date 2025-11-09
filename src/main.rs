e5287ef7dc20d3dee2ea62e4070c8acabb57b7a9
Chetan Gupta <chetanguptaaaa21@gmail.com>
2025-11-09 12:40:57 +0530

    repl

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut command = String::new();
-    io::stdin()
-        .read_line(&mut command)
-        .expect("Failed to read line");
-    let command = command.trim();
-    println!("{command}: command not found");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+        let command = command.trim();
+        println!("{command}: command not found");
+    }
 }
