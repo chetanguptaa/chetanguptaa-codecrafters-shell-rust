e225079fd2d695b5cff7dcfad3796f519265cf7a
Chetan Gupta <chetanguptaaaa21@gmail.com>
2025-11-12 17:44:01 +0530

    letes see

--- src/shell.rs
+++ src/shell.rs
@@ -32,7 +32,7 @@ impl Shell {
     }
     pub fn run(&mut self) -> ShellResult<()> {
         while self.running {
-            println!("$ ");
+            print!("$ ");
             io::stdout().flush()?;
             let mut input = String::new();
             if io::stdin().read_line(&mut input)? == 0 {
