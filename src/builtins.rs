96c31dd4b3b0a39a30fcefc485b63a775ce7d918
Chetan Gupta <chetanguptaaaa21@gmail.com>
2025-11-12 17:35:19 +0530

    removed ln

--- src/builtins.rs
+++ src/builtins.rs
@@ -18,10 +18,10 @@ pub fn get_output_stream(redirect_out: Option<&str>) -> ShellResult<Box<dyn Writ
 pub fn echo(args: &[&str], redirect_out: Option<&str>) -> ShellResult<()> {
     let mut handle = get_output_stream(redirect_out)?;
     if args.is_empty() {
-        writeln!(handle, "")?;
+        write!(handle, "")?;
     } else {
         let output = args.join(" ");
-        writeln!(handle, "{}", output.trim_end())?;
+        write!(handle, "{}", output.trim_end())?;
     }
     Ok(())
 }
@@ -29,7 +29,7 @@ pub fn echo(args: &[&str], redirect_out: Option<&str>) -> ShellResult<()> {
 pub fn pwd(redirect_out: Option<&str>) -> ShellResult<()> {
     let mut handle = get_output_stream(redirect_out)?;
     let dir = env::current_dir()?;
-    writeln!(handle, "{}", dir.display().to_string().trim_end())?;
+    write!(handle, "{}", dir.display().to_string().trim_end())?;
     Ok(())
 }
 
@@ -54,12 +54,12 @@ pub fn r#type(shell: &mut Shell, args: &[&str], redirect_out: Option<&str>) -> S
     };
     let mut handle = get_output_stream(redirect_out)?;
     if shell.builtins.contains(*name) {
-        writeln!(handle, "{name} is a shell builtin")?;
+        write!(handle, "{name} is a shell builtin")?;
         return Ok(());
     }
     match shell.resolve_command(name) {
-        Some(path) => writeln!(handle, "{name} is {}", path.display())?,
-        None => writeln!(handle, "{name}: not found")?,
+        Some(path) => write!(handle, "{name} is {}", path.display())?,
+        None => write!(handle, "{name}: not found")?,
     }
     Ok(())
 }
@@ -72,8 +72,8 @@ pub fn cat(args: &[&str], redirect_out: Option<&str>) -> ShellResult<()> {
     for filename in args {
         let content = std::fs::read_to_string(filename);
         match content {
-            Ok(text) => writeln!(handle, "{}", text)?,
-            Err(_) => writeln!(handle, "cat: {}: No such file or directory", filename)?,
+            Ok(text) => write!(handle, "{}", text)?,
+            Err(_) => write!(handle, "cat: {}: No such file or directory", filename)?,
         }
     }
     Ok(())
