73627458562192a6beca138f3c67f53dcbeaa489
Chetan Gupta <chetanguptaaaa21@gmail.com>
2025-11-16 18:31:42 +0530

    check

--- src/shell.rs
+++ src/shell.rs
@@ -127,6 +127,7 @@ impl Shell {
                             first_tab = false;
                             common_prefix_exists = false;
                         }
+                        stdout.flush()?;
                     }
                     Key::Char(c) => {
                         first_tab = false;
