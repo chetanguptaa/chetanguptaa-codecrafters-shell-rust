4c84f8108810a9355172e21405d997d1835d1eda
Chetan Gupta <chetanguptaaaa21@gmail.com>
2025-11-20 02:03:39 +0530

    -a support fix

--- src/builtins.rs
+++ src/builtins.rs
@@ -195,7 +195,7 @@ pub fn exit(shell: &mut Shell) -> ShellResult<()> {
             .open(file)?;
         let start = 0;
         let end = shell.history.len();
-        for cmd in &shell.history[start - 1..end] {
+        for cmd in &shell.history[start..end] {
             writeln!(file, "{}", cmd)?;
         }
     }
